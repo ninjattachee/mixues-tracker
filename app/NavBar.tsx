7ce5739d14a536122b92505f569d605bd36372da
atank <atank@atank.org>
2024-11-03 00:17:19 +0800

    Style the active link in the navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,6 +1,10 @@
+'use client'
+
 import Link from 'next/link'
+import { usePathname } from 'next/navigation';
 import React from 'react'
 import { BiSolidBug } from "react-icons/bi";
+import classnames from 'classnames';
 
 interface NavLink {
     href: string;
@@ -13,6 +17,7 @@ const navLinks: NavLink[] = [
 ];
 
 const NavBar = () => {
+    const pathname = usePathname();
 
     return (
         <nav className='flex space-x-6 border-b-2 mb-2 px-4 py-2'>
@@ -21,7 +26,17 @@ const NavBar = () => {
             </Link>
             <ul className='flex space-x-4'>
                 {navLinks.map((link) => (
-                    <Link key={link.href} className='text-zinc-500 hover:text-zinc-800 transition-colors duration-200' href={link.href}>
+                    <Link 
+                        key={link.href} 
+                        className={classnames(
+                            'hover:text-zinc-800 transition-colors duration-200',
+                            {
+                                'text-zinc-900': pathname === link.href,
+                                'text-zinc-500': pathname !== link.href
+                            }
+                        )}
+                        href={link.href}
+                    >
                         {link.label}
                     </Link>
                 ))}
