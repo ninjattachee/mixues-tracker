670c6b7e59bbc979a95447a9aec805dbb8555640
atank <atank@atank.org>
2024-11-03 20:52:06 +0800

    Add the edit button

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,9 +1,10 @@
 import IssueStatusBadge from "@/app/components/IssueStatusBadge"
 import prisma from "@/prisma/client"
-import { Button, Card, Flex, Heading, Text } from "@radix-ui/themes"
+import { Box, Button, Card, Flex, Grid, Heading, Text } from "@radix-ui/themes"
 import Link from "next/link"
 import { notFound } from "next/navigation"
 import ReactMarkdown from "react-markdown"
+import { Pencil2Icon } from "@radix-ui/react-icons"
 
 const IssueDetailPage = async ({ params }: { params: { id: string } }) => {
     const issue = await prisma.issue.findUnique({
@@ -13,21 +14,26 @@ const IssueDetailPage = async ({ params }: { params: { id: string } }) => {
     if (!issue) notFound()
 
     return (
-        <div>
-            <Heading>{issue.title}</Heading>
-            <Flex gap='3' my='2'>
-                <IssueStatusBadge status={issue.status} />
-                <Text size='2'>Created: {issue.createdAt.toDateString()}</Text>
-            </Flex>
-            <Card className="prose max-w-full" mt='4'>
-                <ReactMarkdown>{issue.description}</ReactMarkdown>
-            </Card>
-            <Flex className="mt-5 justify-self-end">
-                <Button>
-                    <Link href='.'>Back</Link>
-                </Button>
-            </Flex>
-        </div>
+        <Grid columns='1' gap='5'>
+            <Box>
+                <Heading>{issue.title}</Heading>
+                <Flex gap='3' my='2'>
+                    <IssueStatusBadge status={issue.status} />
+                    <Text size='2'>Created: {issue.createdAt.toDateString()}</Text>
+                </Flex>
+                <Card className="prose max-w-xl" mt='4'>
+                    <ReactMarkdown>{issue.description}</ReactMarkdown>
+                </Card>
+            </Box>
+            <Box>
+                <Flex justify='end' className="max-w-xl">
+                    <Button>
+                        <Pencil2Icon />
+                        <Link href={`/issues/${issue.id}/edit`}>Edit Issue</Link>
+                    </Button>
+                </Flex>
+            </Box>
+        </Grid>
     )
 }
 
