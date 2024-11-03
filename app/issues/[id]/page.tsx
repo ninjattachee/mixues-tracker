1b77e47020751ce66542964c39a1445b2f3f312a
atank <atank@atank.org>
2024-11-03 18:47:00 +0800

    Add markdown preview

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,4 +1,5 @@
 import IssueStatusBadge from "@/app/components/IssueStatusBadge"
+import ReactMarkdown from "react-markdown"
 import prisma from "@/prisma/client"
 import { Button, Card, Flex, Heading, Text } from "@radix-ui/themes"
 import Link from "next/link"
@@ -13,17 +14,19 @@ const IssueDetailPage = async ({ params }: { params: { id: string } }) => {
 
     return (
         <div>
-            <div className="mb-5">
-                <Button>
-                    <Link href='.'>Back</Link>
-                </Button>
-            </div>
             <Heading>{issue.title}</Heading>
             <Flex gap='3' my='2'>
                 <IssueStatusBadge status={issue.status} />
                 <Text size='2'>Created: {issue.createdAt.toDateString()}</Text>
             </Flex>
-            <Card>{issue.description}</Card>
+            <Card className="prose max-w-full" mt='4'>
+                <ReactMarkdown>{issue.description}</ReactMarkdown>
+            </Card>
+            <Flex className="mt-5 justify-self-end">
+                <Button>
+                    <Link href='.'>Back</Link>
+                </Button>
+            </Flex>
         </div>
     )
 }
