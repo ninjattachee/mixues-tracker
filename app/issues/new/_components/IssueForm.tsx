d92cc9b8ae799242a482c222f95d7a430a5afdb6
atank <atank@atank.org>
2024-11-03 13:20:34 +0800

    Handle form submision

--- app/issues/new/_components/IssueForm.tsx
+++ app/issues/new/_components/IssueForm.tsx
@@ -3,6 +3,15 @@
 import { Button, TextField } from '@radix-ui/themes';
 import "easymde/dist/easymde.min.css";
 import dynamic from 'next/dynamic';
+import { Controller, useForm } from 'react-hook-form';
+import axios from 'axios';
+import { useRouter } from 'next/navigation';
+import { toast } from 'react-hot-toast';
+
+interface IssueForm {
+    title: string;
+    description: string;
+}
 
 const SimpleMDE = dynamic(
   () => import('react-simplemde-editor'),
@@ -10,12 +19,23 @@ const SimpleMDE = dynamic(
 );
 
 const IssueForm = () => {
+  const router = useRouter();
+  const { register, handleSubmit, control } = useForm<IssueForm>();
+
+  const onSubmit = (data: IssueForm) => {
+    axios.post('/api/issues', data).then(() => {
+      router.push('/issues');
+    }).catch(() => {
+      toast.error('Failed to create issue.');
+    });
+  }
+
   return (
-    <div className='max-w-xl space-y-3'>
-      <TextField.Root placeholder='Title' />
-      <SimpleMDE placeholder='Description' />
+    <form className='max-w-xl space-y-3' onSubmit={handleSubmit(onSubmit)}>
+      <TextField.Root placeholder='Title' {...register('title', { required: true })} />
+      <Controller control={control} name='description' render={({ field }) => <SimpleMDE placeholder='Description' {...field} />} />
       <Button>Submit New Issue</Button>
-    </div>
+    </form>
   );
 }
 
