e5f6dd1c489712f22176988697a9f6fde30d77b1
atank <atank@atank.org>
2024-11-05 02:14:40 +0800

    Change the layout of the NavBar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -5,7 +5,7 @@ import { usePathname } from 'next/navigation';
 import React from 'react'
 import { BiSolidBug } from "react-icons/bi";
 import classnames from 'classnames';
-import { Box } from '@radix-ui/themes';
+import { Box, Container, Flex } from '@radix-ui/themes';
 import { useSession } from 'next-auth/react';
 
 interface NavLink {
@@ -23,36 +23,42 @@ const NavBar = () => {
     const { status, data: session } = useSession();
 
     return (
-        <nav className='flex space-x-6 border-b-2 mb-2 px-4 py-2'>
-            <Link href="/">
-                <BiSolidBug size={22} color="grey" />
-            </Link>
-            <ul className='flex space-x-4'>
-                {navLinks.map((link) => (
-                    <li key={link.href}>
-                        <Link
-                            key={link.href}
-                            className={classnames(
-                                'hover:text-zinc-800 transition-colors duration-200',
-                                {
-                                    'text-zinc-900': pathname === link.href,
-                                    'text-zinc-500': pathname !== link.href
-                                }
-                            )}
-                            href={link.href}
-                        >
-                            {link.label}
+        <nav className='border-b mb-2 px-4 py-2'>
+            <Container>
+                <Flex justify="between" align="center">
+                    <Flex gap="4">
+                        <Link href="/">
+                            <BiSolidBug size={22} color="grey" />
                         </Link>
-                    </li>
-                ))}
-            </ul>
-            <Box>
-                {
-                    status === 'loading' ? 'Loading...' :
-                        status === 'unauthenticated' ? <Link href="/api/auth/signin">Login</Link> :
-                            <Link href="/api/auth/signout">Logout</Link>
-                }
-            </Box>
+                        <ul className='flex space-x-4'>
+                            {navLinks.map((link) => (
+                                <li key={link.href}>
+                                    <Link
+                                        key={link.href}
+                                        className={classnames(
+                                            'hover:text-zinc-800 transition-colors duration-200',
+                                            {
+                                                'text-zinc-900': pathname === link.href,
+                                                'text-zinc-500': pathname !== link.href
+                                            }
+                                        )}
+                                        href={link.href}
+                                    >
+                                        {link.label}
+                                    </Link>
+                                </li>
+                            ))}
+                        </ul>
+                    </Flex>
+                    <Box>
+                        {
+                            status === 'loading' ? 'Loading...' :
+                                status === 'unauthenticated' ? <Link href="/api/auth/signin">Login</Link> :
+                                    <Link href="/api/auth/signout">Logout</Link>
+                        }
+                    </Box>
+                </Flex>
+            </Container>
         </nav>
     )
 }
