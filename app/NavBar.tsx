49c13d4d39d1c292f6cc753ff6d0f5f77f57dd2f
atank <atank@atank.org>
2024-11-06 15:31:23 +0800

    Filter issues

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,101 +1,109 @@
-'use client'
+"use client";
 
-import Link from 'next/link'
-import { usePathname } from 'next/navigation';
-import React from 'react'
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
 import { BiSolidBug } from "react-icons/bi";
-import classnames from 'classnames';
-import { Avatar, Box, Container, DropdownMenu, Flex, Text } from '@radix-ui/themes';
-import { useSession } from 'next-auth/react';
-import { Skeleton } from '@/app/components';
+import classnames from "classnames";
+import {
+  Avatar,
+  Box,
+  Container,
+  DropdownMenu,
+  Flex,
+  Text,
+} from "@radix-ui/themes";
+import { useSession } from "next-auth/react";
+import { Skeleton } from "@/app/components";
 
 interface NavLink {
-    href: string;
-    label: string;
+  href: string;
+  label: string;
 }
 
 const navLinks: NavLink[] = [
-    { href: "/", label: "Dashboard" },
-    { href: "/issues/list", label: "Issues" },
+  { href: "/", label: "Dashboard" },
+  { href: "/issues/list", label: "Issues" },
 ];
 
 const NavBar = () => {
-
-    return (
-        <nav className='border-b mb-2 px-4 py-2'>
-            <Container>
-                <Flex justify="between" align="center">
-                    <Flex gap="4">
-                        <Link href="/">
-                            <BiSolidBug size={22} color="grey" />
-                        </Link>
-                        <NavLinks />
-                    </Flex>
-                    <AuthStatus />
-                </Flex>
-            </Container>
-        </nav>
-    )
-}
+  return (
+    <nav className="border-b mb-2 px-4 py-2">
+      <Container>
+        <Flex justify="between" align="center">
+          <Flex gap="4">
+            <Link href="/">
+              <BiSolidBug size={22} color="grey" />
+            </Link>
+            <NavLinks />
+          </Flex>
+          <AuthStatus />
+        </Flex>
+      </Container>
+    </nav>
+  );
+};
 
 const NavLinks = () => {
-    const pathname = usePathname();
+  const pathname = usePathname();
 
-    return (
-        <ul className='flex space-x-4'>
-            {navLinks.map((link) => (
-                <li key={link.href}>
-                    <Link
-                        key={link.href}
-                        className={classnames({
-                            'nav-link': true,
-                            '!text-zinc-900': pathname === link.href,
-                        })}
-                        href={link.href}
-                    >
-                        {link.label}
-                    </Link>
-                </li>
-            ))}
-        </ul>
-    )
-}
+  return (
+    <ul className="flex space-x-4">
+      {navLinks.map((link) => (
+        <li key={link.href}>
+          <Link
+            key={link.href}
+            className={classnames({
+              "nav-link": true,
+              "!text-zinc-900": pathname === link.href,
+            })}
+            href={link.href}
+          >
+            {link.label}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 const AuthStatus = () => {
-    const { status, data: session } = useSession();
+  const { status, data: session } = useSession();
 
-    if (status === 'loading') return <Skeleton width="3rem" height="1rem" />
+  if (status === "loading") return <Skeleton width="3rem" height="1rem" />;
 
-    if (status === 'unauthenticated') return <Link className='nav-link' href="/api/auth/signin">Login</Link>
-
+  if (status === "unauthenticated")
     return (
-        <Box>
-            {
-                <DropdownMenu.Root>
-                    <DropdownMenu.Trigger>
-                        <Avatar
-                            src={session?.user?.image || undefined}
-                            fallback="?"
-                            radius="full"
-                            size="2"
-                            className='cursor-pointer'
-                        />
-                    </DropdownMenu.Trigger>
-                    <DropdownMenu.Content>
-                        <DropdownMenu.Label>
-                            <Text>
-                                {session?.user?.email}
-                            </Text>
-                        </DropdownMenu.Label>
-                        <DropdownMenu.Item>
-                            <Link href="/api/auth/signout">Logout</Link>
-                        </DropdownMenu.Item>
-                    </DropdownMenu.Content>
-                </DropdownMenu.Root>
-            }
-        </Box>
-    )
+      <Link className="nav-link" href="/api/auth/signin">
+        Login
+      </Link>
+    );
 
-}
+  return (
+    <Box>
+      {
+        <DropdownMenu.Root>
+          <DropdownMenu.Trigger>
+            <Avatar
+              src={session?.user?.image || undefined}
+              fallback="?"
+              radius="full"
+              size="2"
+              className="cursor-pointer"
+            />
+          </DropdownMenu.Trigger>
+          <DropdownMenu.Content>
+            <DropdownMenu.Label>
+              <Text>{session?.user?.email}</Text>
+            </DropdownMenu.Label>
+            <DropdownMenu.Item>
+              <Link href="/api/auth/signout">Logout</Link>
+            </DropdownMenu.Item>
+          </DropdownMenu.Content>
+        </DropdownMenu.Root>
+      }
+    </Box>
+  );
+};
 
-export default NavBar
+export default NavBar;
--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -2,9 +2,21 @@ import { IssueStatusBadge, Link } from "@/app/components";
 import prisma from "@/prisma/client";
 import { Table } from "@radix-ui/themes";
 import IssueActions from "./IssueActions";
+import { Status } from "@prisma/client";
 
-const IssuesPage = async () => {
-  const issues = await prisma.issue.findMany();
+const IssuesPage = async ({
+  searchParams,
+}: {
+  searchParams: Promise<{ status: Status }>;
+}) => {
+  const { status } = await searchParams;
+  const statuses = Object.values(Status);
+  const statusToFilterBy = statuses.includes(status) ? status : undefined;
+  const issues = await prisma.issue.findMany({
+    where: {
+      status: statusToFilterBy,
+    },
+  });
 
   return (
     <div>
