797750ba2ffee739b291dd861ae40653bfca67f3
atank <atank@atank.org>
2024-11-06 10:21:49 +0800

    Build the filter component

--- app/issues/list/IssueActions.tsx
+++ app/issues/list/IssueActions.tsx
@@ -1,15 +1,17 @@
 import React from 'react'
-import { Button } from '@radix-ui/themes'
+import { Button, Flex } from '@radix-ui/themes'
 import Link from 'next/link'
+import IssueStatusFilter from './IssueStatusFilter';
 
 const IssueActions = () => {
   return (
-    <div className='mb-5'>
+    <Flex mb="5" justify="between">
+      <IssueStatusFilter />
       <Button>
-        <Link href='/issues/new'>Create New Issue</Link>
+        <Link href="/issues/new">Create New Issue</Link>
       </Button>
-    </div>
-  )
-}
+    </Flex>
+  );
+};
 
 export default IssueActions
--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -1,25 +1,26 @@
-import prisma from '@/prisma/client';
-import { Table } from '@radix-ui/themes';
-import { Link, IssueStatusBadge } from '@/app/components';
-import IssueActions from './IssueActions';
+import { IssueStatusBadge, Link } from "@/app/components";
+import prisma from "@/prisma/client";
+import { Table } from "@radix-ui/themes";
+import IssueActions from "./IssueActions";
 
 const IssuesPage = async () => {
-
   const issues = await prisma.issue.findMany();
 
   return (
     <div>
       <IssueActions />
-      <Table.Root variant='surface'>
+      <Table.Root variant="surface">
         <Table.Header>
           <Table.Row>
             <Table.ColumnHeaderCell>Title</Table.ColumnHeaderCell>
             <Table.ColumnHeaderCell>Status</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className='hidden md:table-cell'>Created</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className="hidden md:table-cell">
+              Created
+            </Table.ColumnHeaderCell>
           </Table.Row>
         </Table.Header>
         <Table.Body>
-          {issues.map(issue => (
+          {issues.map((issue) => (
             <Table.Row key={issue.id}>
               <Table.Cell>
                 <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
@@ -27,15 +28,17 @@ const IssuesPage = async () => {
               <Table.Cell>
                 <IssueStatusBadge status={issue.status} />
               </Table.Cell>
-              <Table.Cell className='hidden md:table-cell'>{issue.createdAt.toDateString()}</Table.Cell>
+              <Table.Cell className="hidden md:table-cell">
+                {issue.createdAt.toDateString()}
+              </Table.Cell>
             </Table.Row>
           ))}
         </Table.Body>
       </Table.Root>
     </div>
-  )
-}
+  );
+};
 
-export const dynamic = 'force-dynamic';
+export const dynamic = "force-dynamic";
 
-export default IssuesPage
+export default IssuesPage;
