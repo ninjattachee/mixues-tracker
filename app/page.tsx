d1860e716648916893bb8ced28dfc5aec3a99500
atank <atank@atank.org>
2024-11-08 01:29:23 +0800

    Describing what Mixues is on the dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -2,18 +2,30 @@ import prisma from "@/prisma/client";
 import IssueSummary from "./IssueSummary";
 import LatestIssues from "./LatestIssues";
 import IssueChart from "./IssueChart";
-import { Flex, Grid } from "@radix-ui/themes";
+import { Card, Flex, Grid } from "@radix-ui/themes";
 import { Metadata } from "next";
+import { Text } from "@radix-ui/themes/dist/esm/components/callout.js";
 
 export default async function Home() {
   const open = await prisma.issue.count({ where: { status: "OPEN" } });
-  const inProgress = await prisma.issue.count({ where: { status: "IN_PROGRESS" } });
+  const inProgress = await prisma.issue.count({
+    where: { status: "IN_PROGRESS" },
+  });
   const closed = await prisma.issue.count({ where: { status: "CLOSED" } });
 
   return (
     <Grid columns={{ initial: "1", md: "2" }} gap="5">
       <Flex direction="column" gap="4">
-        <IssueSummary open={open} inProgress={inProgress} closed={closed} />
+        <Flex gap="4" justify="between">
+          <IssueSummary open={open} inProgress={inProgress} closed={closed} />
+            <Text
+              size="2"
+              className="text-muted-foreground max-w-60 hidden sm:block"
+              weight="medium"
+            >
+              Mixues is an issue tracker for Mixin Network and Mixin Messenger
+            </Text>
+        </Flex>
         <IssueChart open={open} inProgress={inProgress} closed={closed} />
       </Flex>
       <LatestIssues />
