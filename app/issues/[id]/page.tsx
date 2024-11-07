f66cc50ca5e6860e6b84fb980a80ff27b42240ad
atank <atank@atank.org>
2024-11-07 12:02:58 +0800

    Optimize performance using react cache

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,55 +1,66 @@
-import prisma from "@/prisma/client"
-import { Box, Flex, Grid } from "@radix-ui/themes"
-import { notFound } from "next/navigation"
-import EditIssueButton from "./EditIssueButton"
-import IssueDetails from "./IssueDetails"
-import DeleteButton from "./DeleteButton"
-import { auth } from "@/app/auth"
-import AsigneeSelect from "./AsigneeSelect"
+import prisma from "@/prisma/client";
+import { Box, Flex, Grid } from "@radix-ui/themes";
+import { notFound } from "next/navigation";
+import EditIssueButton from "./EditIssueButton";
+import IssueDetails from "./IssueDetails";
+import DeleteButton from "./DeleteButton";
+import { auth } from "@/app/auth";
+import AsigneeSelect from "./AsigneeSelect";
+import { cache } from "react";
 
-const IssueDetailPage = async ({ params }: { params: Promise<{ id: string }> }) => {
-    const { id } = await params;
-    const issue = await prisma.issue.findUnique({
-        where: { id: parseInt(id) }
-    })
+const fetchIssue = cache((issueId: number) =>
+  prisma.issue.findUnique({ where: { id: issueId } })
+);
 
-    if (!issue) { notFound() }
+const IssueDetailPage = async ({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) => {
+  const { id } = await params;
+  const issue = await fetchIssue(parseInt(id));
 
-    const session = await auth();
+  if (!issue) {
+    notFound();
+  }
 
-    return (
-        <Grid columns={{ initial: '1', sm: '5' }} gap='5'>
-            <Box className="md:col-span-4">
-                <IssueDetails issue={issue} />
-            </Box>
-            <Box>
-                {
-                    session && (
-                        <Flex direction='column' justify='center' gap='4' className="max-w-full">
-                            <AsigneeSelect issue={issue} />
-                            <EditIssueButton issueId={issue.id} />
-                            <DeleteButton issueId={issue.id} />
-                        </Flex>
-                    )
-                }
-            </Box>
-        </Grid>
-    )
+  const session = await auth();
+
+  return (
+    <Grid columns={{ initial: "1", sm: "5" }} gap="5">
+      <Box className="md:col-span-4">
+        <IssueDetails issue={issue} />
+      </Box>
+      <Box>
+        {session && (
+          <Flex
+            direction="column"
+            justify="center"
+            gap="4"
+            className="max-w-full"
+          >
+            <AsigneeSelect issue={issue} />
+            <EditIssueButton issueId={issue.id} />
+            <DeleteButton issueId={issue.id} />
+          </Flex>
+        )}
+      </Box>
+    </Grid>
+  );
+};
+
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+  const issue = await fetchIssue(parseInt(id));
+
+  return {
+    title: issue?.title,
+    description: issue?.description,
+  };
 }
 
-export async function generateMetadata({ params }: { params: { id: string } }) {
-    const issue = await prisma.issue.findUnique({
-        where: { id: parseInt(params.id) }
-    })
-
-    if (!issue) {
-        notFound()
-    }
-
-    return {
-        title: issue?.title,
-        description: issue?.description
-    }
-}
-
-export default IssueDetailPage
+export default IssueDetailPage;
